4e26d69f949257394075a4b28cd197b498095375
antonmok <anton.mokienko1488@gmail.com>
2021-03-02 15:33:02 +0300

    First time boost tests used

---
+++ tests/algo.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE test module name
+#include <boost/test/included/unit_test.hpp>
+
+
+
+BOOST_AUTO_TEST_CASE( jwt_usage_test )
+{
+
+  BOOST_TEST( true );
+}
---
+++ tests/api.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE test module name
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( api_login_test )
+{
+
+  BOOST_TEST( true );
+}
+
+BOOST_AUTO_TEST_CASE( api_settings_test )
+{
+
+  BOOST_TEST( true );
+}
